665b58b47f01bd21a8519d3a4117b96e259822e9
kihyoun.lee <kihyoun.lee@goorm.io>
2018-12-20 10:25:26 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -1,40 +1,14 @@
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-console.log('test');
-	console.log('test');
+function add(a, b) {
+	const ret = a + b;
+	return ret;
+}
+
+function add2(a, b) {
+	const ret = a + b;
+	return a + ret;
+}
+
+function add3(a, b) {
+	const ret = a + b;
+	return a + ret;
+}
